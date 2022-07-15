4c9b09cba7ee8a17e2020307cf6aca30797f3b59
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2022-07-15 16:21:09 +0200

    PDEDef and BoundaryConditions are actually inner functions, inner to LMAHeureuxPorosityDiffV2, so I changed the indentation, that seemed to have been converted incorrectly by matlab2python. Also a 'return c,f,s' seemed to have resulted from an incorrect translation.

--- LMAHeureuxPorosityDiffV2.py
+++ LMAHeureuxPorosityDiffV2.py
@@ -43,53 +43,53 @@ def LMAHeureuxPorosityDiffV2(AragoniteInitial = None,CalciteInitial = None,CaIni
     presum = 1 - rhorat0 * Phi0 ** 3 * (1 - np.exp(10 - 10 / Phi0)) / (1 - Phi0)
     
     ## Define Initial conditions
-    InitialConditions = lambda depth = None: np.array([[AragoniteInitial(depth)],[CalciteInitial(depth)],[CaInitial(depth)],[CO3Initial(depth)],[PorInitial(depth)]])
+    InitialConditions = lambda depth = None: np.array([[AragoniteInitial(depth)],[CalciteInitial(depth)],
+                                                       [CaInitial(depth)],[CO3Initial(depth)],[PorInitial(depth)]])
     ## Define Boundary conditions
     
-def BoundaryConditions(AragoniteSurface, CalciteSurface, CaSurface, CO3Surface, PorSurface, ul, t): 
-    #eq. 35 top
-    ql = np.array([[0],[0],[0],[0],[0]])
-    pl = np.array([[ul(1) - AragoniteSurface(t)],[ul(2) - CalciteSurface(t)],[ul(3) - CaSurface(t)],
-        [ul(4) - CO3Surface(t)],[ul(5) - PorSurface(t)]])
-    pr = np.array([[0],[0],[0],[0],[0]])
-    # eq 35 bottom
-    qr = np.array([[1],[1],[1],[1],[1]])
-    return pl,ql,pr,qr
-    
+    def BoundaryConditions(AragoniteSurface, CalciteSurface, CaSurface, CO3Surface, PorSurface, ul, t): 
+        #eq. 35 top
+        ql = np.array([[0],[0],[0],[0],[0]])
+        pl = np.array([[ul(1) - AragoniteSurface(t)],[ul(2) - CalciteSurface(t)],[ul(3) - CaSurface(t)],
+            [ul(4) - CO3Surface(t)],[ul(5) - PorSurface(t)]])
+        pr = np.array([[0],[0],[0],[0],[0]])
+        # eq 35 bottom
+        qr = np.array([[1],[1],[1],[1],[1]])
+        return pl,ql,pr,qr
+        
     ## Define System of PDEs
-    
-def PDEDef(x = None,__ = None,u = None,dudx = None): 
-    ##System of PDEs of LHeureux, described in eqs. 40 to 43
-#abbreciations for readability
-    CA = u(1)
-    CC = u(2)
-    cCa = u(3)
-    cCO3 = u(4)
-    Phi = u(5)
-    #formulas for compact representation
-#dPhi=(auxcon*((Phi^3)/(1-Phi))*(1-exp(10-10/Phi))); # eq. 25 + 17 in comb with eq. 44
-    dPhislash = (auxcon * (Phi / ((1 - Phi) ** 2)) * (np.exp(10 - 10 / Phi) * (2 * Phi ** 2 + 7 * Phi - 10) + Phi * (3 - 2 * Phi)))
-    #OmegaPA=max(0,cCa*cCO3*KRat-1)^m1; #eq. 45
-#OmegaDA=(max(0,1-cCa*cCO3*KRat)^m2)*(x*Xstar <= DeepLimit && x*Xstar >= ShallowLimit); #eq. 45
-#OmegaPC=max(0,cCa*cCO3-1)^n1; #eq. 45
-#OmegaDC=max(0,1-cCa*cCO3)^n2; #eq. 45
-    coA = CA * (((np.amax(0,1 - cCa * cCO3 * KRat) ** m2) * (x * Xstar <= DeepLimit and x * Xstar >= ShallowLimit)) - nu1 * (np.amax(0,cCa * cCO3 * KRat - 1) ** m1))
-    coC = CC * ((np.amax(0,cCa * cCO3 - 1) ** n1) - nu2 * (np.amax(0,1 - cCa * cCO3) ** n2))
-    U = (presum + rhorat * Phi ** 3 * (1 - np.exp(10 - 10 / Phi)) / (1 - Phi))
-    
-    W = (presum - rhorat * Phi ** 2 * (1 - np.exp(10 - 10 / Phi)))
-    
-    Wslash = - rhorat * 2 * (Phi - (Phi + 5) * np.exp(10 - 10 / Phi))
-    #Describe eqs. 40 to 43
-    c = np.array([[1],[1],[Phi],[Phi],[1]])
-    
-    f = np.array([[0],[0],[Phi * dCa * dudx(3)],[Phi * dCO3 * dudx(4)],[(auxcon * ((Phi ** 3) / (1 - Phi)) * (1 - np.exp(10 - 10 / Phi))) * dudx(5)]])
-    
-    s = np.array([[(- U * dudx(1) - Da * ((1 - CA) * coA + lambda_ * CA * coC))],[(- U * dudx(2) + Da * (lambda_ * (1 - CC) * coC + CC * coA))],[(- Phi * W * dudx(3) + Da * (1 - Phi) * (delta - cCa) * (coA - lambda_ * coC))],[(- Phi * W * dudx(4) + Da * (1 - Phi) * (delta - cCO3) * (coA - lambda_ * coC))],[(Da * (1 - Phi) * (coA - lambda_ * coC) - dudx(5) * (W + Wslash * Phi + dudx(5) * dPhislash))]])
-    return c,f,s
+    def PDEDef(x = None,__ = None,u = None,dudx = None): 
+        ##System of PDEs of LHeureux, described in eqs. 40 to 43
+        #abbreciations for readability
+        CA = u(1)
+        CC = u(2)
+        cCa = u(3)
+        cCO3 = u(4)
+        Phi = u(5)
+        #formulas for compact representation
+    #dPhi=(auxcon*((Phi^3)/(1-Phi))*(1-exp(10-10/Phi))); # eq. 25 + 17 in comb with eq. 44
+        dPhislash = (auxcon * (Phi / ((1 - Phi) ** 2)) * (np.exp(10 - 10 / Phi) * (2 * Phi ** 2 + 7 * Phi - 10) + Phi * (3 - 2 * Phi)))
+        #OmegaPA=max(0,cCa*cCO3*KRat-1)^m1; #eq. 45
+    #OmegaDA=(max(0,1-cCa*cCO3*KRat)^m2)*(x*Xstar <= DeepLimit && x*Xstar >= ShallowLimit); #eq. 45
+    #OmegaPC=max(0,cCa*cCO3-1)^n1; #eq. 45
+    #OmegaDC=max(0,1-cCa*cCO3)^n2; #eq. 45
+        coA = CA * (((np.amax(0,1 - cCa * cCO3 * KRat) ** m2) * (x * Xstar <= DeepLimit and x * Xstar >= ShallowLimit)) - nu1 * (np.amax(0,cCa * cCO3 * KRat - 1) ** m1))
+        coC = CC * ((np.amax(0,cCa * cCO3 - 1) ** n1) - nu2 * (np.amax(0,1 - cCa * cCO3) ** n2))
+        U = (presum + rhorat * Phi ** 3 * (1 - np.exp(10 - 10 / Phi)) / (1 - Phi))
+        
+        W = (presum - rhorat * Phi ** 2 * (1 - np.exp(10 - 10 / Phi)))
+        
+        Wslash = - rhorat * 2 * (Phi - (Phi + 5) * np.exp(10 - 10 / Phi))
+        #Describe eqs. 40 to 43
+        c = np.array([[1],[1],[Phi],[Phi],[1]])
+        
+        f = np.array([[0],[0],[Phi * dCa * dudx(3)],[Phi * dCO3 * dudx(4)],[(auxcon * ((Phi ** 3) / (1 - Phi)) * (1 - np.exp(10 - 10 / Phi))) * dudx(5)]])
+        
+        s = np.array([[(- U * dudx(1) - Da * ((1 - CA) * coA + lambda_ * CA * coC))],[(- U * dudx(2) + Da * (lambda_ * (1 - CC) * coC + CC * coA))],[(- Phi * W * dudx(3) + Da * (1 - Phi) * (delta - cCa) * (coA - lambda_ * coC))],[(- Phi * W * dudx(4) + Da * (1 - Phi) * (delta - cCO3) * (coA - lambda_ * coC))],[(Da * (1 - Phi) * (coA - lambda_ * coC) - dudx(5) * (W + Wslash * Phi + dudx(5) * dPhislash))]])
+        return c,f,s
     
     ## Solve PDE
     sol = pdepe(0,PDEDef,InitialConditions,BoundaryConditions,xmesh,tspan,options)
-    return c,f,s
+    # return c,f,s
     
     return sol
