7d7b0b30a49223eb4c0e79e64e07d61c371ed58b
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2023-06-15 17:21:14 +0200

    First step to making the main script modular. Will make implementing the regression test easier.

--- Evolve_scenario.py
+++ Evolve_scenario.py
@@ -9,58 +9,71 @@ import matplotlib.pyplot as plt
 from LHeureux_model import LMAHeureuxPorosityDiff
 from marlpde.marlpde import Scenario, Solver
 
-Scenario_parameters = Scenario()
-Solver_parameters = Solver()
+def read_Scenario_parameters():
+    '''
+    '''
+    Scenario_parameters = Scenario()
+    
+    KA = Scenario_parameters.Ka.magnitude
+    KC = Scenario_parameters.Kc.magnitude
+    CA0 = Scenario_parameters.cara0.magnitude
+    CAIni = Scenario_parameters.cara00.magnitude
+    CC0 = Scenario_parameters.ccal0.magnitude
+    CCIni = Scenario_parameters.ccal00.magnitude
+    cCa0 = Scenario_parameters.ca0.magnitude/np.sqrt(KC)
+    cCaIni = Scenario_parameters.ca00.magnitude/np.sqrt(KC)
+    cCO30 = Scenario_parameters.co30.magnitude/np.sqrt(KC)
+    cCO3Ini = Scenario_parameters.co300.magnitude/np.sqrt(KC)
+    Phi0 = Scenario_parameters.phi0.magnitude
+    PhiIni = Scenario_parameters.phi00.magnitude
+    
+    ShallowLimit = Scenario_parameters.xdis.magnitude
+    
+    DeepLimit = ShallowLimit + Scenario_parameters.Th.magnitude
+    
+    sedimentationrate = Scenario_parameters.S.magnitude
+    m1 = Scenario_parameters.m.magnitude
+    m2 = m1
+    n1 = Scenario_parameters.nn.magnitude
+    n2 = n1
+    rhoa = Scenario_parameters.rhoa.magnitude
+    rhoc = Scenario_parameters.rhoc.magnitude
+    rhot = Scenario_parameters.rhot.magnitude
+    rhos0 = rhoa * CA0 + rhoc * CC0 + rhot * (1 - (CA0 + CC0))
+    
+    rhos = rhos0
+    
+    rhow = Scenario_parameters.rhow.magnitude
+    beta = Scenario_parameters.beta.magnitude
+    D0Ca = Scenario_parameters.D0ca.magnitude
+    k1 = Scenario_parameters.k1.magnitude
+    k2 = Scenario_parameters.k2.magnitude
+    k3 = Scenario_parameters.k3.magnitude
+    k4 = Scenario_parameters.k4.magnitude
+    muA = Scenario_parameters.mua.magnitude
+    DCa = Scenario_parameters.D0ca.magnitude
+    DCO3 = Scenario_parameters.D0co3.magnitude
+    b = Scenario_parameters.b.magnitude/1e4
+    PhiNR = Scenario_parameters.phi00.magnitude
+    PhiInfty = Scenario_parameters.phiinf.magnitude
+    
+    Xstar = D0Ca / sedimentationrate
+    Tstar = Xstar / sedimentationrate 
+    
+    max_depth = Scenario_parameters.length.magnitude
 
-KA = Scenario_parameters.Ka.magnitude
-KC = Scenario_parameters.Kc.magnitude
-CA0 = Scenario_parameters.cara0.magnitude
-CAIni = Scenario_parameters.cara00.magnitude
-CC0 = Scenario_parameters.ccal0.magnitude
-CCIni = Scenario_parameters.ccal00.magnitude
-cCa0 = Scenario_parameters.ca0.magnitude/np.sqrt(KC)
-cCaIni = Scenario_parameters.ca00.magnitude/np.sqrt(KC)
-cCO30 = Scenario_parameters.co30.magnitude/np.sqrt(KC)
-cCO3Ini = Scenario_parameters.co300.magnitude/np.sqrt(KC)
-Phi0 = Scenario_parameters.phi0.magnitude
-PhiIni = Scenario_parameters.phi00.magnitude
+    return all_Scenario_parameters
 
-ShallowLimit = Scenario_parameters.xdis.magnitude
+def read_solver_parameters():
+    '''
+    '''
+    Solver_parameters = Solver()
+    NUMBER_OF_DEPTHS = Solver_parameters.N
+    end_time = Solver_parameters.tmax/Tstar
+    time_step = Solver_parameters.dt
+    number_of_steps = end_time/time_step
 
-DeepLimit = ShallowLimit + Scenario_parameters.Th.magnitude
-
-sedimentationrate = Scenario_parameters.S.magnitude
-m1 = Scenario_parameters.m.magnitude
-m2 = m1
-n1 = Scenario_parameters.nn.magnitude
-n2 = n1
-rhoa = Scenario_parameters.rhoa.magnitude
-rhoc = Scenario_parameters.rhoc.magnitude
-rhot = Scenario_parameters.rhot.magnitude
-rhos0 = rhoa * CA0 + rhoc * CC0 + rhot * (1 - (CA0 + CC0))
-
-rhos = rhos0
-
-rhow = Scenario_parameters.rhow.magnitude
-beta = Scenario_parameters.beta.magnitude
-D0Ca = Scenario_parameters.D0ca.magnitude
-k1 = Scenario_parameters.k1.magnitude
-k2 = Scenario_parameters.k2.magnitude
-k3 = Scenario_parameters.k3.magnitude
-k4 = Scenario_parameters.k4.magnitude
-muA = Scenario_parameters.mua.magnitude
-DCa = Scenario_parameters.D0ca.magnitude
-DCO3 = Scenario_parameters.D0co3.magnitude
-b = Scenario_parameters.b.magnitude/1e4
-PhiNR = Scenario_parameters.phi00.magnitude
-PhiInfty = Scenario_parameters.phiinf.magnitude
-
-Xstar = D0Ca / sedimentationrate
-Tstar = Xstar / sedimentationrate 
-
-max_depth = Scenario_parameters.length.magnitude
-
-NUMBER_OF_DEPTHS = Solver_parameters.N
+    return all_solver_parameters
 
 depths = CartesianGrid([[0, max_depth/Xstar]], [NUMBER_OF_DEPTHS], periodic=False)
 # We will be needing forward and backward differencing for
@@ -90,10 +103,6 @@ eq = LMAHeureuxPorosityDiff(AragoniteSurface, CalciteSurface, CaSurface,
                             k3, k4, m1, m2, n1, n2, b, beta, rhos, rhow, rhos0, 
                             KA, KC, muA, D0Ca, PhiNR, PhiInfty, PhiIni, DCa, DCO3, 
                             not_too_shallow, not_too_deep)             
-
-end_time = Solver_parameters.tmax/Tstar
-time_step = Solver_parameters.dt
-number_of_steps = end_time/time_step
 
 state = eq.get_state(AragoniteSurface, CalciteSurface, CaSurface, 
                      CO3Surface, PorSurface)
@@ -125,3 +134,9 @@ plt.ylabel("Compositions and concentrations (dimensionless)")
 plt.legend(loc='upper right')
 plt.plot()
 plt.show()
+
+if __name__ == '__main__':
+    scenario_parameters = read_Scenario_parameters()
+    solver_parameters = read_solver_parameters()
+    solution = solve_stuff()
+    plot = plot_stuff()
