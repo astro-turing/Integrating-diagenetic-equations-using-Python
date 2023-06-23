e2024b921cfd8e0735ab37f1adc43fefeb7f9c25
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2023-06-23 17:19:43 +0200

    Still a bit if struggle back and forth figuring out how this large number of Scenario parameters should be passed along in the most readable way. They could be given as an argument to integrate_equations or integrate_equations could retrieve them himself. Not sure yet. That set of Scenario parameters has to be handed over to LMAHeureuxPorosityDiff.

--- Evolve_scenario.py
+++ Evolve_scenario.py
@@ -2,86 +2,103 @@
 
 from datetime import datetime
 import os
+from dataclasses import asdict
+import matplotlib.pyplot as plt
 from pde import CartesianGrid, ScalarField, FileStorage
 from pde.grids.operators.cartesian import _make_derivative
-import matplotlib.pyplot as plt
 from LHeureux_model import LMAHeureuxPorosityDiff
 from marlpde.marlpde import Map_Scenario, Solver
 
-def retrieve_solver_parameters():
+def integrate_equations():
     '''
-    Function to retrieve the solver parameters defined in marlpde.py.
+    This function retrieves the parameters of the Scenario to be simulated and 
+    the solution parameters for the integration. It then integrates the five
+    partial differential equations form L'Heureux, stores and returns the 
+    solution, to be used for plotting.
     '''
+    Scenario_parameters = Map_Scenario()
+    Xstar = Scenario_parameters.Xstar
+    Tstar = Scenario_parameters.Tstar
+    max_depth = Scenario_parameters.max_depth
+    ShallowLimit = Scenario_parameters.ShallowLimit
+    DeepLimit = Scenario_parameters.ShallowLimit
+    CAIni = Scenario_parameters.CAIni
+    CCIni = Scenario_parameters.CCIni
+    cCaIni = Scenario_parameters.cCaIni
+    cCO3Ini = Scenario_parameters.cCO3Ini
+    PhiIni = Scenario_parameters.PhiIni
+
     Solver_parameters = Solver()
-    NUMBER_OF_DEPTHS = Solver_parameters.N
-    end_time = Solver_parameters.tmax/Tstar
-    time_step = Solver_parameters.dt
-    number_of_steps = end_time/time_step
+    Number_of_depths = Solver_parameters.N
+    # End_time is in units of Tstar.
+    End_time = Solver_parameters.tmax/Tstar
+    dt = Solver_parameters.dt
 
-    return all_solver_parameters
+    depths = CartesianGrid([[0, max_depth/Xstar]], [Number_of_depths], periodic=False)
+    # We will be needing forward and backward differencing for
+    # Fiadeiro-Veronis differentiation.
+    depths.register_operator("grad_back", \
+        lambda grid: _make_derivative(grid, method="backward"))
+    depths.register_operator("grad_forw", \
+        lambda grid: _make_derivative(grid, method="forward"))
+    
+    AragoniteSurface = ScalarField(depths, CAIni)
+    CalciteSurface = ScalarField(depths, CCIni)
+    CaSurface = ScalarField(depths, cCaIni)
+    CO3Surface = ScalarField(depths, cCO3Ini)
+    PorSurface = ScalarField(depths, PhiIni)
+    
+    # I need those two fields for computing coA, which is rather involved.
+    # There may be a simpler way of selecting these depths, but I haven't
+    # found one yet. For now these two Heaviside step functions.
+    not_too_shallow = ScalarField.from_expression(depths,
+                      f"heaviside(x-{ShallowLimit/Xstar}, 0)")
+    not_too_deep = ScalarField.from_expression(depths,
+                   f"heaviside({DeepLimit/Xstar}-x, 0)")    
+    
+    eq = LMAHeureuxPorosityDiff(AragoniteSurface, CalciteSurface, CaSurface, 
+                                CO3Surface, PorSurface, not_too_shallow, 
+                                not_too_deep, **asdict(Scenario_parameters))             
+    
+    state = eq.get_state(AragoniteSurface, CalciteSurface, CaSurface, 
+                         CO3Surface, PorSurface)
+    
+    # Store your results somewhere in a subdirectory of a parent directory.
+    store_folder = "../Results/" + datetime.now().strftime("%d_%m_%Y_%H_%M_%S" + "/")
+    os.makedirs(store_folder)
+    stored_results = store_folder + "LMAHeureuxPorosityDiff.hdf5"
+    storage = FileStorage(stored_results)
+    
+    sol, info = eq.solve(state, t_range=End_time, dt=dt, method="explicit", \
+                   scheme = "rk", tracker=["progress", storage.tracker(0.01)], \
+                   backend = "numba", ret_info = True, adaptive = True)
+    print()
+    print(f"Meta-information about the solution : {info}")        
 
-depths = CartesianGrid([[0, max_depth/Xstar]], [NUMBER_OF_DEPTHS], periodic=False)
-# We will be needing forward and backward differencing for
-# Fiadeiro-Veronis differentiation.
-depths.register_operator("grad_back", \
-    lambda grid: _make_derivative(grid, method="backward"))
-depths.register_operator("grad_forw", \
-    lambda grid: _make_derivative(grid, method="forward"))
+    covered_time = Tstar * end_time
 
-AragoniteSurface = ScalarField(depths, CAIni)
-CalciteSurface = ScalarField(depths, CCIni)
-CaSurface = ScalarField(depths, cCaIni)
-CO3Surface = ScalarField(depths, cCO3Ini)
-PorSurface = ScalarField(depths, PhiIni)
+    return sol, covered_time
 
-# I need those two fields for computing coA, which is rather involved.
-# There may be a simpler way of selecting these depths, but I haven't
-# found one yet. For now these two Heaviside step functions.
-not_too_shallow = ScalarField.from_expression(depths,
-                  f"heaviside(x-{ShallowLimit/Xstar}, 0)")
-not_too_deep = ScalarField.from_expression(depths,
-               f"heaviside({DeepLimit/Xstar}-x, 0)")    
-
-eq = LMAHeureuxPorosityDiff(AragoniteSurface, CalciteSurface, CaSurface, 
-                            CO3Surface, PorSurface, CA0, CC0, cCa0, cCO30, 
-                            Phi0, sedimentationrate, Xstar, Tstar, k1, k2, 
-                            k3, k4, m1, m2, n1, n2, b, beta, rhos, rhow, rhos0, 
-                            KA, KC, muA, D0Ca, PhiNR, PhiInfty, PhiIni, DCa, DCO3, 
-                            not_too_shallow, not_too_deep)             
-
-state = eq.get_state(AragoniteSurface, CalciteSurface, CaSurface, 
-                     CO3Surface, PorSurface)
-
-# Store your results somewhere in a subdirectory of a parent directory.
-store_folder = "../Results/" + datetime.now().strftime("%d_%m_%Y_%H_%M_%S" + "/")
-os.makedirs(store_folder)
-stored_results = store_folder + "LMAHeureuxPorosityDiff.hdf5"
-storage = FileStorage(stored_results)
-
-sol, info = eq.solve(state, t_range=end_time, dt=time_step, method="explicit", \
-               scheme = "rk", tracker=["progress", storage.tracker(0.01)], \
-               backend = "numba", ret_info = True, adaptive = True)
-print()
-print(f"Meta-information about the solution : {info}")        
-
-covered_time = Tstar * end_time
-plt.title(f"Situation after {covered_time:.2f} years")
-# Marker size
-ms = 3
-plotting_depths = ScalarField.from_expression(depths, "x").data * Xstar
-plt.plot(plotting_depths, sol.data[0], "v", ms = ms, label = "CA")
-plt.plot(plotting_depths, sol.data[1], "^", ms = ms, label = "CC")
-plt.plot(plotting_depths, sol.data[2], ">", ms = ms, label = "cCa")
-plt.plot(plotting_depths, sol.data[3], "<", ms = ms, label = "cCO3")
-plt.plot(plotting_depths, sol.data[4], "o", ms = ms, label = "Phi")
-plt.xlabel("Depth (cm)")
-plt.ylabel("Compositions and concentrations (dimensionless)")
-plt.legend(loc='upper right')
-plt.plot()
-plt.show()
+def Plot_results(sol, covered_time):
+    '''
+    Plot the five fields at the end of the integration interval as a function
+    of depth.
+    '''
+    plt.title(f"Situation after {covered_time:.2f} years")
+    # Marker size
+    ms = 3
+    plotting_depths = ScalarField.from_expression(depths, "x").data * Xstar
+    plt.plot(plotting_depths, sol.data[0], "v", ms = ms, label = "CA")
+    plt.plot(plotting_depths, sol.data[1], "^", ms = ms, label = "CC")
+    plt.plot(plotting_depths, sol.data[2], ">", ms = ms, label = "cCa")
+    plt.plot(plotting_depths, sol.data[3], "<", ms = ms, label = "cCO3")
+    plt.plot(plotting_depths, sol.data[4], "o", ms = ms, label = "Phi")
+    plt.xlabel("Depth (cm)")
+    plt.ylabel("Compositions and concentrations (dimensionless)")
+    plt.legend(loc='upper right')
+    plt.plot()
+    plt.show()
 
 if __name__ == '__main__':
-    scenario_parameters = read_Scenario_parameters()
-    solver_parameters = read_solver_parameters()
-    solution = solve_stuff()
-    plot = plot_stuff()
+    solution, covered_time = integrate_equations()
+    Plot_results(solution, covered_time)
