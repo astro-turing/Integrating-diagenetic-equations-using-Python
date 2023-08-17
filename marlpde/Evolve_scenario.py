186fef3ad4683ab8b09aa7799857205504231971
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2023-08-17 17:01:57 +0200

    It seemed interesting to not only show the final distributions, but also how they change over time, updated in this case every 5% of the final integration time span. And save the final figure as a pdf. Plotting the final distributions needed some recoding or it would be using the figure object created by LivePlotTracker and the final distributions would be plotted on top of the rightmost panel of that figure object.

--- marlpde/Evolve_scenario.py
+++ marlpde/Evolve_scenario.py
@@ -5,7 +5,7 @@ import os
 from dataclasses import asdict
 import inspect
 import matplotlib.pyplot as plt
-from pde import CartesianGrid, ScalarField, FileStorage
+from pde import CartesianGrid, ScalarField, FileStorage, LivePlotTracker
 from pde.grids.operators.cartesian import _make_derivative
 from parameters import Map_Scenario, Solver
 from LHeureux_model import LMAHeureuxPorosityDiff
@@ -71,47 +71,54 @@ def integrate_equations(**kwargs):
                          CO3Surface, PorSurface)
     
     # Store your results somewhere in a subdirectory of a parent directory.
-    store_folder = "../Results/" + datetime.now().strftime("%d_%m_%Y_%H_%M_%S" + "/")
+    store_folder = "../Results/" + \
+                   datetime.now().strftime("%d_%m_%Y_%H_%M_%S" + "/")
     os.makedirs(store_folder)
     stored_results = store_folder + "LMAHeureuxPorosityDiff.hdf5"
     storage = FileStorage(stored_results, info=kwargs)
+    live_plots = LivePlotTracker(interval='0:05', title="Integration results",
+                                 show=True, max_fps=1, \
+                                 plot_args ={"ax_style": {"ylim": (0, 1.5)}})
     
     sol, info = eq.solve(state, t_range=End_time, dt=dt, \
                          solver=kwargs["solver"], scheme=kwargs["scheme"],\
                          tracker=["progress", \
-                         storage.tracker(kwargs["tracker_interval"])], \
+                         storage.tracker(kwargs["tracker_interval"]),\
+                         live_plots], \
                          backend=kwargs["backend"], ret_info=kwargs["retinfo"],\
                          adaptive=kwargs["adaptive"])
+
     print()
     print(f"Meta-information about the solution : {info}")        
 
     covered_time = Tstar * End_time
 
-    return sol, covered_time, depths, Xstar
+    return sol, covered_time, depths, Xstar, store_folder
 
-def Plot_results(sol, covered_time, depths, Xstar):
+def Plot_results(sol, covered_time, depths, Xstar, store_folder):
     '''
     Plot the five fields at the end of the integration interval as a function
     of depth.
     '''
-    plt.title(f"Situation after {covered_time:.2f} years")
+    fig, ax = plt.subplots()
+    fig.suptitle(f"Situation after {covered_time:.2f} years")
     # Marker size
     ms = 5
     plotting_depths = ScalarField.from_expression(depths, "x").data * Xstar
-    plt.plot(plotting_depths, sol.data[0], "v", ms = ms, label = "CA")
-    plt.plot(plotting_depths, sol.data[1], "^", ms = ms, label = "CC")
-    plt.plot(plotting_depths, sol.data[2], ">", ms = ms, label = "cCa")
-    plt.plot(plotting_depths, sol.data[3], "<", ms = ms, label = "cCO3")
-    plt.plot(plotting_depths, sol.data[4], "o", ms = ms, label = "Phi")
-    plt.xlabel("Depth (cm)")
-    plt.ylabel("Compositions and concentrations (dimensionless)")
-    plt.legend(loc='upper right')
-    plt.show()
+    ax.plot(plotting_depths, sol.data[0], "v", ms = ms, label = "CA")
+    ax.plot(plotting_depths, sol.data[1], "^", ms = ms, label = "CC")
+    ax.plot(plotting_depths, sol.data[2], ">", ms = ms, label = "cCa")
+    ax.plot(plotting_depths, sol.data[3], "<", ms = ms, label = "cCO3")
+    ax.plot(plotting_depths, sol.data[4], "o", ms = ms, label = "Phi")
+    ax.set_xlabel("Depth (cm)")
+    ax.set_ylabel("Compositions and concentrations (dimensionless)")
+    ax.legend(loc='upper right')
+    fig.savefig(store_folder + 'Final_distributions.pdf', bbox_inches="tight")
 
 if __name__ == '__main__':
     # Concatenate the dict containing the Scenario parameters with the
     # dict containing the solver parameters (such as required tolerance).
     all_kwargs = asdict(Map_Scenario()) | asdict(Solver())
-    solution, covered_time, depths, Xstar = \
+    solution, covered_time, depths, Xstar, store_folder = \
         integrate_equations(**all_kwargs)
-    Plot_results(solution, covered_time, depths, Xstar)
+    Plot_results(solution, covered_time, depths, Xstar, store_folder)
