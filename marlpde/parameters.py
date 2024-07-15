a9691a179a7d9c2d22c4dcbf9c6af5d27a92a9a6
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2024-07-15 11:57:25 +0200

    outt and outx are redundant. They are remnants from the original Python dataclass. We want to use the scipy solver, i.e. solve_ivp, since it provides for implicit methods. Also we want to integrate over 250_000 years, so we should set the progress tracker interval accordingly.

--- marlpde/parameters.py
+++ marlpde/parameters.py
@@ -158,13 +158,10 @@ class Solver:
     eps: float    = 1.e-2
     # T* is more suitable as a default value
     # than the original value (100_000 years).
-    tmax: int     = 30 * Map_Scenario().Tstar
-    # timesteps in between writing.
-    outt: int     =   1_000
-    outx: int     =  25_000
+    tmax: int     = 2.5e5
     N: int        = 200
-    solver: str   = "explicit"
-    scheme: str   = "rk"
+    solver: str   = "scipy"
+    scheme: str   = "euler"
     backend: str = "numba"
     retinfo: bool = True
     adaptive: bool = True
@@ -175,7 +172,7 @@ class Tracker:
     Initialises all the tracking parameters, such as tracker interval.
     Also indicates the quantities to be tracked, as boolean values.
     '''
-    progress_tracker_interval: float = 0.01
+    progress_tracker_interval: float = 2.5e2/13190
     live_plotting: bool = False
     plotting_interval: str = '0:05'
     data_tracker_interval: float = 0.01
