c14d03dc1f8d440847723cb79711ce414286a79a
Hanno Spreeuw <h.spreeuw@esciencecenter.nl>
2024-10-07 16:16:43 +0200

    Expanded docstring for integrate_equations, covering descriptions of input and output. Trying to comply with PEP 257, e.g. start with a one-line summary of what the function does. covered_time should correspond to (end_time - t0) not to end_time (although t0 is probably zero).

--- marlpde/Evolve_scenario.py
+++ marlpde/Evolve_scenario.py
@@ -17,11 +17,54 @@ import matplotlib.pyplot as plt
 matplotlib.use("AGG")
 
 def integrate_equations(solver_parms, tracker_parms, pde_parms):
-    '''
+    '''Perform the integration and display and store the results.
+
     This function retrieves the parameters of the Scenario to be simulated and 
     the solution parameters for the integration. It then integrates the five
     partial differential equations from L'Heureux, stores and returns the 
-    solution, to be used for plotting.
+    solution, to be used for plotting. Its input comes from the parameters
+    module, which has dataclasses governing the solver, storage and model specs,
+    through the solver_parms, tracker_parms and pde_parms dicts, respectively.
+
+    A progress bar shows how long the (remaining) integration will take.
+
+    Parameters:
+    -----------
+    solver_parms: dict 
+        Parameters about solver settings.
+    tracker_parms: dict 
+        Parameters about the progress bar and time interval for storage
+    pde_parms: dict
+        Model parameters, which govern e.g. the physical processes, but
+        also the discretization, such as the number of grid cells.
+
+    Returns:
+    --------
+    field_solutions: ndarray
+        This is the "y" attribute of "sol" i.e. the solution derived by
+        solve_ivp. See the scipy.integrate.solve_ivp documentation for
+        some background. A reshape has been applied to arrive at one row per
+        field. The solutions as a function of time have been removed such that
+        only the solution for the last time is returned.
+
+    covered_time: float
+        This is the time interval of integration in years, from the start time
+        (probably 0) until the requested final time. When the integration halted
+        unexpectedly, the covered_time corresponds to the time covered until the
+        integration halted.
+
+    depths: pde.CartesianGrid
+        These are the centers of the grid cells that together constitute the 
+        grid.
+    
+    Xstar: float
+        Scaling factor between physical depths and dimensionless depths as used
+        in the differential equations.
+
+    Store_folder: str
+        Could be a relative path, i.e.  a path relative to the root of this 
+        repository, to a folder where solutions of the integration should be 
+        stored. Could also be an absolute path, though.
     '''
 
     Xstar = pde_parms["Xstar"]
@@ -63,7 +106,7 @@ def integrate_equations(solver_parms, tracker_parms, pde_parms):
 
     slices_all_fields = [slice(i * Number_of_depths, (i+1) * Number_of_depths) \
                              for i in range(5)]            
-    
+
     eq = LMAHeureuxPorosityDiff(depths, slices_all_fields, not_too_shallow, 
                                 not_too_deep, **filtered_pde_parms)     
     
@@ -149,9 +192,9 @@ def integrate_equations(solver_parms, tracker_parms, pde_parms):
           f"{end_computing - start_computing:.2e}s. \n"))
     
     if sol.status == 0:
-        covered_time = Tstar * end_time
+        covered_time = Tstar * (end_time - t0)
     else:
-        covered_time = pbar.n * Tstar * end_time / no_progress_updates 
+        covered_time = pbar.n * Tstar * (end_time - t0) / no_progress_updates 
 
     # Store your results somewhere in a subdirectory of a parent directory.
     store_folder = "../Results/" + \
